12ff5d980529d8dbcf90a439883871e5879793b9
be-krishna <bkshgnj@gmail.com>
2023-10-13 13:54:58 +0530

    part4b-ex-4.20 done | add tokenextracor mdw

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '');
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {
     username: 1,
@@ -22,7 +14,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const currUser = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+  const currUser = jwt.verify(request.token, process.env.SECRET);
 
   if (!currUser.id) {
     return response.status(401).json({ error: 'invalid token' });
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,16 +1,28 @@
 const logger = require('./logger');
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error);
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: error.message });
   }
 
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    const token = authorization.replace('Bearer ', '');
+    request.token = token;
+  }
+
+  next();
+};
+
 module.exports = {
   errorHandler,
+  tokenExtractor,
 };
