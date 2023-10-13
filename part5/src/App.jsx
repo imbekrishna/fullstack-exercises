9dae5650eb777f4cb8f2eec3765790f790e629d2
be-krishna <bkshgnj@gmail.com>
2023-10-13 20:18:03 +0530

    inital commit

---
+++ part5/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
+
+import blogService from "./services/blogs";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      {user === null ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ part5/src/components/LoginForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <>
+      <h1>log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
