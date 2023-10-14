f1ad6cf0cb5476f4850ea360f324be1d502b6473
be-krishna <bkshgnj@gmail.com>
2023-10-14 14:14:56 +0530

    part5-ex-5.10 done | add sort by like

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -91,9 +91,11 @@ const App = () => {
           <Togglable buttonLabel="create new blog" ref={blogFormRef}>
             <BlogForm addBlog={addBlog} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
+            ))}
         </div>
       )}
     </div>
