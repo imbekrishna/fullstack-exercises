c60575ee21665be3017c4ad8f3b3d2896948fcad
imbekrishna <imbekrishna@gmail.com>
2023-10-18 02:27:23 +0530

    part7 ex-7.14 done

--- part7/bloglist-redux/src/components/Users.jsx
+++ part7/bloglist-redux/src/components/Users.jsx
@@ -9,7 +9,27 @@ const Users = () => {
   useEffect(() => {
     dispatch(getSummary());
   }, []);
-  return <div>Users</div>;
+  return (
+    <div>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {summary.map((e) => (
+            <tr key={e._id}>
+              <td>{e.name}</td>
+              <td>{e.blogCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
